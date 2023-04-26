b390bbcfac5319180d2c146924ee497d24f640dc
henrickBL <jhbl1@aluno.ifal.edu.br>
2023-04-26 17:22:05 -0300

    Adicionando projeto da aula do dia 26/04/2023

---
+++ 04.26/projetohenrique923/android/app/src/main/kotlin/com/example/projetohenrique923/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projetohenrique923
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 04.26/projetohenrique923/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:projetohenrique923/tela_login.dart';
+
+void main() {
+  runApp(
+    MaterialApp(home: TelaPrincipal()),
+  );
+}
